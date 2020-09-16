6a30fc60db2ab2628bffa7698ae900d9e7952caa
Three-year-old <songjie19979016@163.com>
2020-09-16 11:01:26 +0800

    217. Contains Duplicate

---
+++ src/Array/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return true;
+            } else {
+                map.put(num, 1);
+            }
+        }
+        return false;
+    }
+}
