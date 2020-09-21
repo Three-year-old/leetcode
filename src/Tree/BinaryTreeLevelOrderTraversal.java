2ffa3274ac602048c0ac06c2d3d5c2ed337affda
Three-year-old <songjie19970916@163.com>
2020-09-21 19:56:58 +0800

    102. Binary Tree Level Order Traversal

---
+++ src/Tree/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,35 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int length = queue.size();
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = 0; i < length; i++) {
+                TreeNode poll = queue.poll();
+                assert poll != null;
+                tmp.add(poll.val);
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+            }
+            ans.add(tmp);
+        }
+        return ans;
+    }
+}
