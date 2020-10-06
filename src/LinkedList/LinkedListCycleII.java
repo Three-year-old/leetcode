828ef7e0b92a11fcfcbfbcc8cc2e26cc0637c2de
Three-year-old <songjie19979016@163.com>
2020-10-06 15:32:28 +0800

    142. Linked List Cycle II

---
+++ src/LinkedList/LinkedListCycleII.java
@@ -0,0 +1,26 @@
+package LinkedList;
+
+public class LinkedListCycleII {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null) {
+            fast = fast.next;
+            if (fast == null) {
+                return null;
+            }
+            fast = fast.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode p = head;
+                while (p != slow) {
+                    p = p.next;
+                    slow = slow.next;
+                }
+                return p;
+            }
+        }
+        return null;
+    }
+}
