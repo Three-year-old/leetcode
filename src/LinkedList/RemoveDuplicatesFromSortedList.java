8f4a2bef6c8de99b85e8e424d20ec215d5dd0099
Three-year-old <songjie19979016@163.com>
2020-10-06 15:16:18 +0800

    83. Remove Duplicates from Sorted List

---
+++ src/LinkedList/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class RemoveDuplicatesFromSortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode p = head;
+        while (p != null) {
+            while (p.next != null && p.val == p.next.val) {
+                p.next = p.next.next;
+            }
+            p = p.next;
+        }
+        return head;
+    }
+}
