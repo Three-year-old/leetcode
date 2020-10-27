a515e383a915b28e8313965141615a0365f36b6a
Three-year-old <songjie19970916@163.com>
2020-10-27 12:22:35 +0800

    143. Reorder List

---
+++ src/LinkedList/ReorderList.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReorderList {
+
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        List<ListNode> link = new ArrayList<>();
+        ListNode p = head;
+        while (p != null) {
+            link.add(p);
+            p = p.next;
+        }
+        for (int i = 0; i < link.size() / 2; i++) {
+            link.get(i).next = link.get(link.size() - 1 - i);
+            if (i != (link.size() / 2 - 1)) {
+                link.get(link.size() - 1 - i).next = link.get(i + 1);
+            } else {
+                if (link.size() %2 == 0) {
+                    link.get(link.size() - 1 - i).next = null;
+                } else {
+                    link.get(link.size() - 1 - i).next = link.get(link.size() / 2);
+                    link.get(link.size() / 2).next = null;
+                }
+            }
+        }
+        head = link.get(0);
+    }
+}
