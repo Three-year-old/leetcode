dc76d858c1d8c6bd8f73088938fd841a1c13037e
Three-year-old <songjie19970916@163.com>
2020-11-06 14:26:36 +0800

    717. 1-bit and 2-bit Characters

---
+++ src/Array/OneBitAndTwoBitCharacters.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class OneBitAndTwoBitCharacters {
+
+    public boolean isOneBitCharacter(int[] bits) {
+        int ones = 0;
+        for (int i = bits.length - 2; i >= 0 && bits[i] != 0; i--) {
+            ones++;
+        }
+        return ones % 2 <= 0;
+    }
+}
