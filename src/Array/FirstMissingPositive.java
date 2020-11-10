045f0b9a2825b9dcb271096b33c944a99699df0f
Three-year-old <songjie19970916@163.com>
2020-11-10 09:44:08 +0800

    41. First Missing Positive

---
+++ src/Array/FirstMissingPositive.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Arrays;
+
+public class FirstMissingPositive {
+
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            //当nums[i]-1不越界时
+            //将当前nums[i]放到该放的地方
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+}
