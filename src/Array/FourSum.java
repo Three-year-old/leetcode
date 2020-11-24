e71dff911f5ed9679b19eecb10d647cc43aef88f
Three-year-old <songjie19970916@163.com>
2020-11-24 14:27:34 +0800

    18. 4Sum

---
+++ src/Array/FourSum.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    for (int l = k + 1; l < nums.length; l++) {
+                        if (nums[i] + nums[j] + nums[k] + nums[l] == target) {
+                            List<Integer> list = new ArrayList<>();
+                            list.add(nums[i]);
+                            list.add(nums[j]);
+                            list.add(nums[k]);
+                            list.add(nums[l]);
+                            if (!res.contains(list))
+                                res.add(list);
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
