017a49175388137256d73ba6d2e481133c726806
Three-year-old <songjie19970916@163.com>
2021-01-21 13:21:19 +0800

    11. Container With Most Water

--- src/Array/ContainerWithMostWater.java
+++ src/Array/ContainerWithMostWater.java
@@ -3,16 +3,14 @@ package Array;
 public class ContainerWithMostWater {
 
     public int maxArea(int[] height) {
-        int n = height.length;//n > 1
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            //计算以第i个柱子为左边，所能装的最大水
-            int res = 0;
-            for (int j = i; j < n; j++) {
-                res = Math.max(res, (j - i) * Math.min(height[j], height[i]));
-            }
-            max = Math.max(res, max);
+        int left = 0;
+        int right = height.length - 1;
+        int water = 0;
+        while (left < right) {
+            water = Math.max(water, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] < height[right]) left++;
+            else right--;
         }
-        return max;
+        return water;
     }
 }
