dff355097b7ea7ad88857052111d6264b89d7d77
surzia <songjie19970916@163.com>
2021-02-01 20:42:15 +0800

    621. Task Scheduler

---
+++ src/Greedy/TaskScheduler.java
@@ -0,0 +1,22 @@
+package Greedy;
+
+import java.util.Arrays;
+
+public class TaskScheduler {
+
+    public int leastInterval(char[] tasks, int n) {
+        int[] buckets = new int[26];
+        for (char task : tasks) {
+            buckets[task - 'A']++;
+        }
+        Arrays.sort(buckets);
+        int maxTimes = buckets[25];
+        int maxCount = 1;
+        for (int i = 25; i >= 1; i--) {
+            if (buckets[i] == buckets[i - 1]) maxCount++;
+            else break;
+        }
+        int res = (maxTimes - 1) * (n + 1) + maxCount;
+        return Math.max(res, tasks.length);
+    }
+}
