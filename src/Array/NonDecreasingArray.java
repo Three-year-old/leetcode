ff080d4d627120859d30c2934645c4fcd65068b8
surzia <songjie19970916@163.com>
2021-02-07 09:25:58 +0800

    665. Non-decreasing Array

--- src/Array/NonDecreasingArray.java
+++ src/Array/NonDecreasingArray.java
@@ -3,27 +3,14 @@ package Array;
 public class NonDecreasingArray {
 
     public boolean checkPossibility(int[] nums) {
-        int p = 0;
         int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i + 1 < nums.length && nums[i] > nums[i + 1]) {
-                p = i + 1;
+        for (int i = 1; i < nums.length && count <= 1; i++) {
+            if (nums[i - 1] > nums[i]) {
                 count++;
+                if (i - 2 < 0 || nums[i - 2] <= nums[i]) nums[i - 1] = nums[i];
+                else nums[i] = nums[i - 1];
             }
         }
-        if (p == 0) {
-            return true;
-        }
-        if (count > 1) {
-            return false;
-        }
-        int right = Integer.MAX_VALUE, left = Integer.MIN_VALUE;
-        if (p + 1 < nums.length) {
-            right = nums[p + 1];
-        }
-        if (p - 2 >= 0) {
-            left = nums[p - 2];
-        }
-        return Math.max(nums[p], nums[p - 1]) <= right && Math.max(nums[p], nums[p - 1]) >= left || Math.min(nums[p], nums[p - 1]) <= right && Math.min(nums[p], nums[p - 1]) >= left;
+        return count <= 1;
     }
 }
