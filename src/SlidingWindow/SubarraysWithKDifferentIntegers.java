79cb6eaefc4f7b0397e8c04bcca1764e6d469e3d
surzia <songjie19970916@163.com>
2021-02-09 12:36:54 +0800

    992. Subarrays with K Different Integers

---
+++ src/SlidingWindow/SubarraysWithKDifferentIntegers.java
@@ -0,0 +1,40 @@
+package SlidingWindow;
+
+public class SubarraysWithKDifferentIntegers {
+
+    public int subarraysWithKDistinct(int[] A, int K) {
+        int n = A.length;
+        int[] num1 = new int[n + 1];
+        int[] num2 = new int[n + 1];
+        int tot1 = 0, tot2 = 0;
+        int left1 = 0, left2 = 0, right = 0;
+        int ret = 0;
+        while (right < n) {
+            if (num1[A[right]] == 0) {
+                tot1++;
+            }
+            num1[A[right]]++;
+            if (num2[A[right]] == 0) {
+                tot2++;
+            }
+            num2[A[right]]++;
+            while (tot1 > K) {
+                num1[A[left1]]--;
+                if (num1[A[left1]] == 0) {
+                    tot1--;
+                }
+                left1++;
+            }
+            while (tot2 > K - 1) {
+                num2[A[left2]]--;
+                if (num2[A[left2]] == 0) {
+                    tot2--;
+                }
+                left2++;
+            }
+            ret += left2 - left1;
+            right++;
+        }
+        return ret;
+    }
+}
