87153abe3cf92cd97d5564f78bed625d1ae63801
surzia <songjie19970916@163.com>
2021-03-11 20:50:10 +0800

    3. Longest Substring Without Repeating Characters

--- src/String/LongestSubstringWithoutRepeatingCharacters.java
+++ src/String/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,31 +1,21 @@
 package String;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
-        //从第一个字符开始
-        //每次维护一个Set
-        //当往后找的字符出现在Set时
-        //清空Set并记录该值
-        int max = 0;
-        int record = 0;
-        int i = 0;
-        Set<Character> set = new HashSet<>();
-        while (i < s.length()) {
-            int p = i;
-            while (p < s.length() && !set.contains(s.charAt(p))) {
-                set.add(s.charAt(p));
-                p++;
-                record++;
+        if (s.length() == 0) return 0;
+        int left = 0, right = 0, res = 0;
+        int[] status = new int[128];
+        while (right < s.length()) {
+            if (status[s.charAt(right)] == 0) {
+                status[s.charAt(right)]++;
+                right++;
+            } else {
+                status[s.charAt(left)]--;
+                left++;
             }
-            set.clear();
-            max = Math.max(max, record);
-            record = 0;
-            i++;
+            res = Math.max(res, right - left);
         }
-        return max;
+        return res;
     }
 }
