c54e2b875d4bdf2d132e837dbd36e5cf9426456e
surzia <songjie19970916@163.com>
2021-03-12 14:44:01 +0800

    31. Next Permutation

--- src/Array/NextPermutation.java
+++ src/Array/NextPermutation.java
@@ -1,29 +1,39 @@
 package Array;
 
-import java.util.Arrays;
-
 public class NextPermutation {
 
     public void nextPermutation(int[] nums) {
-        int i;
-        for (i = nums.length - 1; i >= 0; i--) {
-            if (i != 0 && nums[i] > nums[i - 1]) {
+        if (nums.length == 0) return;
+        int large = -1;
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                large = i - 1;
                 break;
             }
         }
-        if (i <= 0) {
-            Arrays.sort(nums);
+        if (large == -1) {
+            reverse(nums, 0, nums.length - 1);
             return;
         }
-        int j;
-        for (j = nums.length; j >= i; j--) {
-            if (nums[j] > nums[i - 1]) {
-                break;
-            }
+        int next = nums.length - 1;
+        while (nums[next] <= nums[large]) {
+            next--;
         }
-        int temp = nums[j];
-        nums[j] = nums[i - 1];
-        nums[i - 1] = temp;
-        Arrays.sort(nums, i, nums.length);
+        swap(nums, large, next);
+        reverse(nums, large + 1, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int c = nums[a];
+        nums[a] = nums[b];
+        nums[b] = c;
     }
 }
