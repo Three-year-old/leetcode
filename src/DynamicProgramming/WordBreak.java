85f83624f8dd07328edc862ff00d4f79865caf8b
surzia <songjie19970916@163.com>
2021-03-16 22:11:40 +0800

    139. Word Break

--- src/DynamicProgramming/WordBreak.java
+++ src/DynamicProgramming/WordBreak.java
@@ -1,24 +1,23 @@
 package DynamicProgramming;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WordBreak {
 
     public boolean wordBreak(String s, List<String> wordDict) {
-        return search(s, 0, wordDict);
-    }
-
-    private boolean search(String s, int length, List<String> wordDict) {
-        if (length == s.length()) {
-            return true;
-        }
-        for (int i = 0; i < wordDict.size(); i++) {
-            if (s.startsWith(wordDict.get(i), length)) {
-                if (search(s, length + wordDict.get(i).length(), wordDict)) {
-                    return true;
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-        return false;
+        return dp[s.length()];
     }
 }
