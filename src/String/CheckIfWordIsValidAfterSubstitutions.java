593dbb8909b771eec8305ea16af4f546dfa7a43e
surzia <songjie19970916@163.com>
2021-03-18 20:44:44 +0800

    1003. Check If Word Is Valid After Substitutions

--- src/String/CheckIfWordIsValidAfterSubstitutions.java
+++ src/String/CheckIfWordIsValidAfterSubstitutions.java
@@ -1,13 +1,19 @@
 package String;
 
+import java.util.Stack;
+
 public class CheckIfWordIsValidAfterSubstitutions {
 
     public boolean isValid(String s) {
-        if (s.length() < 2) return false;
-        if (s.equals("abc")) return true;
-        String rep = s.replace("abc", "");
-        if (rep.equals(s)) return false;
-        if (rep.equals("")) return true;
-        return isValid(rep);
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == 'c') {
+                if (stack.isEmpty() || stack.pop() != 'b') return false;
+                if (stack.isEmpty() || stack.pop() != 'a') return false;
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
     }
 }
