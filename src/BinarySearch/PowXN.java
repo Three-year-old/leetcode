052d2b957692468294c18dd622da4e92abc66024
surzia <songjie19970916@163.com>
2021-03-21 15:35:05 +0800

    50. Pow(x, n)

---
+++ src/BinarySearch/PowXN.java
@@ -0,0 +1,12 @@
+package BinarySearch;
+
+public class PowXN {
+
+    public double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;
+        double res = myPow(x, n / 2);
+        return res * res * myPow(x, n % 2);
+    }
+}
