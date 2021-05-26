16bf410271abc551387c9a4fc850c64079a1ae88
surzia <songjie19970916@163.com>
2021-05-26 16:03:30 +0800

    394. Decode String

--- src/Stack/DecodeString.java
+++ src/Stack/DecodeString.java
@@ -5,32 +5,37 @@ import java.util.Stack;
 public class DecodeString {
 
     public String decodeString(String s) {
-        StringBuilder res = new StringBuilder();
-        Stack<Integer> count = new Stack<>();
-        Stack<String> stack = new Stack<>();
+        String res = "";
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> resStack = new Stack<>();
         int idx = 0;
         while (idx < s.length()) {
             if (Character.isDigit(s.charAt(idx))) {
-                int num = 0;
+                int count = 0;
                 while (Character.isDigit(s.charAt(idx))) {
-                    num = 10 * num + (s.charAt(idx) - '0');
+                    count = 10 * count + (s.charAt(idx) - '0');
                     idx++;
                 }
-                count.push(num);
-            } else if (s.charAt(idx) == '[') {
-                stack.push(res.toString());
-                res = new StringBuilder();
+                countStack.push(count);
+            }
+            else if (s.charAt(idx) == '[') {
+                resStack.push(res);
+                res = "";
                 idx++;
-            } else if (s.charAt(idx) == ']') {
-                StringBuilder builder = new StringBuilder(stack.pop());
-                Integer pop = count.pop();
-                builder.append(res.toString().repeat(Math.max(0, pop)));
-                res = builder;
+            }
+            else if (s.charAt(idx) == ']') {
+                StringBuilder temp = new StringBuilder (resStack.pop());
+                int repeatTimes = countStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    temp.append(res);
+                }
+                res = temp.toString();
                 idx++;
-            } else {
-                res.append(s.charAt(idx++));
+            }
+            else {
+                res += s.charAt(idx++);
             }
         }
-        return res.toString();
+        return res;
     }
 }
