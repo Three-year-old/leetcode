710455caf499bc1ab9ae6ed1c9dca5f97fcf9ff1
surzia <songjie19970916@163.com>
2021-05-31 22:50:40 +0800

    461. Hamming Distance

---
+++ src/BitManipulation/HammingDistance.java
@@ -0,0 +1,13 @@
+package BitManipulation;
+
+public class HammingDistance {
+
+    public int hammingDistance(int x, int y) {
+        int s = x ^ y, c = 0;
+        while (s != 0) {
+            c += s & 1;
+            s >>= 1;
+        }
+        return c;
+    }
+}
