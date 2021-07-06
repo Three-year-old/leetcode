251c38f0bb528d8d6466fb54b5c5f6272bdab67e
surzia <songjie19970916@163.com>
2021-07-06 22:28:17 +0800

    128. Longest Consecutive Sequence

--- src/Array/LongestConsecutiveSequence.java
+++ src/Array/LongestConsecutiveSequence.java
@@ -1,50 +1,25 @@
 package Array;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 
 public class LongestConsecutiveSequence {
 
     public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) return 0;
-        UnionFind uf = new UnionFind(nums);
+        HashSet<Integer> set = new HashSet<>();
         for (int num : nums) {
-            uf.merge(num, num + 1);
+            set.add(num);
         }
-        int max = 1;
+
+        int res = 0;
         for (int num : nums) {
-            Integer find = uf.find(num);
-            assert find != null;
-            max = Math.max(max, find - num + 1);
-        }
-        return max;
-    }
-
-    static class UnionFind {
-        private final Map<Integer, Integer> map;
-
-        public UnionFind(int[] p) {
-            map = new HashMap<>();
-            for (int i : p) {
-                map.put(i, i);
+            if (!set.contains(num - 1)) {
+                int len = 1;
+                while (set.contains(num + len)) {
+                    len++;
+                }
+                res = Math.max(res, len);
             }
         }
-
-        private Integer find(int k) {
-            if (!map.containsKey(k)) return null;
-            int root = k;
-            while (root != map.get(root)) {
-                root = map.get(root);
-            }
-            return root;
-        }
-
-        private void merge(int x, int y) {
-            Integer p = find(x);
-            Integer q = find(y);
-            if (p == null || q == null) return;
-            if (p.equals(q)) return;
-            map.put(p, q);
-        }
+        return res;
     }
 }
