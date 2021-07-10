74e1835a37c3597b77d9f764895158bd158f9094
surzia <songjie19970916@163.com>
2021-07-10 15:16:35 +0800

    88. Merge Sorted Array

--- src/Array/MergeSortedArray.java
+++ src/Array/MergeSortedArray.java
@@ -1,33 +1,22 @@
 package Array;
 
-import java.util.Arrays;
-
 public class MergeSortedArray {
 
     private void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (m == 0) {
-            if (n >= 0) System.arraycopy(nums2, 0, nums1, 0, n);
-        }
-        while (m > 0 && n > 0) {
-            if (nums1[m - 1] > nums2[n - 1]) {
-                nums1[m + n - 1] = nums1[m - 1];
-                m--;
+        int p1 = m - 1, p2 = n - 1;
+        int tail = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] <= nums2[p2]) {
+                nums1[tail--] = nums2[p2--];
             } else {
-                nums1[m + n - 1] = nums2[n - 1];
-                n--;
+                nums1[tail--] = nums1[p1--];
             }
         }
-        while (n > 0) {
-            nums1[n - 1] = nums2[n - 1];
-            n--;
+        while (p1 >= 0) {
+            nums1[tail--] = nums1[p1--];
         }
-    }
-
-    public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int[] nums2 = {2, 5, 6};
-        MergeSortedArray m = new MergeSortedArray();
-        m.merge(nums1, 3, nums2, 3);
-        System.out.println(Arrays.toString(nums1));
+        while (p2 >= 0) {
+            nums1[tail--] = nums2[p2--];
+        }
     }
 }
